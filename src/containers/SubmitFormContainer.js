54d855bb59894ac5f43e864d9bd9dbbf434b312d
ottocedeno <otto@creativespaceclub.com>
2020-11-12 17:49:42 -0500

    Converted form compotent to class component and initialized state

--- src/containers/SubmitFormContainer.js
+++ src/containers/SubmitFormContainer.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import FormTextInput from "../components/FormTextInput";
 
-const SubmitFormContainer = () => {
-  return (
-    <form className="px-4 mt-8">
-      <input
-        type="text"
-        name="Video Title"
-        placeholder="Video Title"
-        className="border border-black rounded-full outline-none px-4 py-3 w-full mb-8"
-      />
-      <input
-        type="text"
-        name="Quick Description"
-        placeholder="Quick Description"
-        className="border border-black rounded-full outline-none px-4 py-3 w-full mb-8"
-      />
-      <input
-        type="text"
-        name="YouTube Link"
-        placeholder="YouTube Link"
-        className="border border-black rounded-full outline-none px-4 py-3 w-full mb-8"
-      />
-    </form>
-  );
-};
+class SubmitFormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      camera: "Mevo Start",
+    };
+  }
+
+  render() {
+    return (
+      <form className="px-4 mt-8">
+        <FormTextInput name="title" placeholder="Video Title" />
+        <FormTextInput name="description" placeholder="Quick Description" />
+        <p className="text-sm text-center mb-2">
+          Please submit 1080p HD YouTube clip
+        </p>
+        <FormTextInput name="YouTubeLink" placeholder="YouTube Link" />
+        <p className="text-sm text-center mb-2">
+          Emails are not shared publicly
+        </p>
+        <FormTextInput name="email" placeholder="Contact Email" />
+
+        <p className="text-center uppercase font-bold text-sm">
+          Footage shot with
+        </p>
+        <div className="border border-red-600 flex justify-center space-x-8">
+          <label>
+            <input
+              type="radio"
+              name="cameraType"
+              value="mevoStart"
+              defaultChecked
+            />
+            Mevo Start
+          </label>
+          <label>
+            <input type="radio" name="cameraType" value="mevoCore" />
+            Mevo Core
+          </label>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default SubmitFormContainer;
