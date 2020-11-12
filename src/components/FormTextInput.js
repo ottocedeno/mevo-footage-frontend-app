0debf5dc102d2aae8b845ac4d01ae3eb96846d29
ottocedeno <otto@creativespaceclub.com>
2020-11-12 18:16:42 -0500

    Added props and focus styling

--- src/components/FormTextInput.js
+++ src/components/FormTextInput.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const FormTextInput = ({ name, placeholder }) => {
+const FormTextInput = ({ name, placeholder, value, handleTextInputChange }) => {
   return (
     <input
       type="text"
       name={name}
       placeholder={placeholder}
-      className="border border-black rounded-full outline-none px-4 py-3 w-full mb-8"
+      value={value}
+      onChange={handleTextInputChange}
+      className="focus:shadow-lg focus:border-black border focus:opacity-100 border-gray-500 rounded-full outline-none px-4 py-3 w-full mb-8"
     />
   );
 };
