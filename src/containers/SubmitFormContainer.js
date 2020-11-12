2a37a0d26578beacfea6c287372aee83e31969a5
ottocedeno <otto@creativespaceclub.com>
2020-11-12 18:28:36 -0500

    Added form controls to all the text inputs

--- src/containers/SubmitFormContainer.js
+++ src/containers/SubmitFormContainer.js
@@ -5,23 +5,60 @@ class SubmitFormContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      title: "",
+      description: "",
+      youtube_url: "",
+      user_email: "",
       camera: "Mevo Start",
     };
   }
 
+  handleTextInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <form className="px-4 mt-8">
-        <FormTextInput name="title" placeholder="Video Title" />
-        <FormTextInput name="description" placeholder="Quick Description" />
+      <form className="px-4 mt-8" onSubmit={this.handleFormSubmit}>
+        <FormTextInput
+          name="title"
+          placeholder="Video Title"
+          value={this.state.title}
+          handleTextInputChange={this.handleTextInputChange}
+        />
+
+        <FormTextInput
+          name="description"
+          placeholder="Quick Description"
+          value={this.state.description}
+          handleTextInputChange={this.handleTextInputChange}
+        />
+
         <p className="text-sm text-center mb-2">
           Please submit 1080p HD YouTube clip
         </p>
-        <FormTextInput name="YouTubeLink" placeholder="YouTube Link" />
+        <FormTextInput
+          name="youtube_url"
+          placeholder="YouTube Link"
+          value={this.state.youtube_url}
+          handleTextInputChange={this.handleTextInputChange}
+        />
         <p className="text-sm text-center mb-2">
           Emails are not shared publicly
         </p>
-        <FormTextInput name="email" placeholder="Contact Email" />
+        <FormTextInput
+          name="user_email"
+          placeholder="Contact Email"
+          value={this.state.user_email}
+          handleTextInputChange={this.handleTextInputChange}
+        />
 
         <p className="text-center uppercase font-bold text-sm">
           Footage shot with
@@ -40,6 +77,13 @@ class SubmitFormContainer extends Component {
             <input type="radio" name="cameraType" value="mevoCore" />
             Mevo Core
           </label>
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-red-600 text-white w-full py-3 rounded-full"
+          />
         </div>
       </form>
     );
