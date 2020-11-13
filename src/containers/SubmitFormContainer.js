7ba04d4e131232d02ad2e7c987b1171250dc21c6
ottocedeno <otto@creativespaceclub.com>
2020-11-12 20:30:53 -0500

    Added categories radio selection

--- src/containers/SubmitFormContainer.js
+++ src/containers/SubmitFormContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormTextInput from "../components/FormTextInput";
 import FormRadioInput from "../components/FormRadioInput";
+import FormSubmitButton from "../components/FormSubmitButton";
 
 class SubmitFormContainer extends Component {
   constructor(props) {
@@ -11,8 +12,21 @@ class SubmitFormContainer extends Component {
       youtube_url: "",
       user_email: "",
       model: "Mevo Start",
+      category_name: this.categories[0],
     };
   }
+
+  categories = [
+    "House of Worship",
+    "Sports",
+    "Government",
+    "Conferences",
+    "Education",
+    "Gaming",
+    "Fitness",
+    "Hobbies",
+    "Music",
+  ];
 
   handleFormInputChange = (event) => {
     this.setState({
@@ -25,23 +39,21 @@ class SubmitFormContainer extends Component {
     console.log(this.state);
   };
 
-  render() {
+  renderTextInputs = () => {
     return (
-      <form className="px-4 mt-8" onSubmit={this.handleFormSubmit}>
+      <div>
         <FormTextInput
           name="title"
           placeholder="Video Title"
           value={this.state.title}
           handleFormInputChange={this.handleFormInputChange}
         />
-
         <FormTextInput
           name="description"
           placeholder="Quick Description"
           value={this.state.description}
           handleFormInputChange={this.handleFormInputChange}
         />
-
         <p className="text-sm text-center mb-2">
           Please submit 1080p HD YouTube clip
         </p>
@@ -60,30 +72,65 @@ class SubmitFormContainer extends Component {
           value={this.state.user_email}
           handleFormInputChange={this.handleFormInputChange}
         />
+      </div>
+    );
+  };
 
+  renderCameraSelector = () => {
+    return (
+      <div>
         <p className="text-center uppercase font-bold text-sm">
           Footage shot with
         </p>
-        <div className="flex justify-center space-x-8 mt-2 mb-4">
+        <div className="flex flex-col mt-2 mb-4">
           <FormRadioInput
             name="model"
             value={"Mevo Start"}
-            currentModel={this.state.model}
+            current={this.state.model}
             handleFormInputChange={this.handleFormInputChange}
           />
           <FormRadioInput
             name="model"
             value={"Mevo Core"}
-            currentModel={this.state.model}
+            current={this.state.model}
             handleFormInputChange={this.handleFormInputChange}
           />
         </div>
+      </div>
+    );
+  };
+
+  renderCategorySelection = () => {
+    return (
+      <div>
+        <p className="text-center uppercase font-bold text-sm">
+          category (select one)
+        </p>
+        <div className="flex mt-2 mb-4 flex-col pl-4">
+          {this.categories.map((category, id) => {
+            return (
+              <FormRadioInput
+                key={id}
+                name="category_name"
+                value={category}
+                current={this.state.category_name}
+                handleFormInputChange={this.handleFormInputChange}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <form className="px-4 mt-8" onSubmit={this.handleFormSubmit}>
+        {this.renderTextInputs()}
+        {this.renderCameraSelector()}
+        {this.renderCategorySelection()}
         <div>
-          <input
-            type="submit"
-            value="Submit"
-            className="bg-red-600 text-white w-full py-3 rounded-full"
-          />
+          <FormSubmitButton />
         </div>
       </form>
     );
