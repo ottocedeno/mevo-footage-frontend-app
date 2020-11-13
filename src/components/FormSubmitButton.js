e1ab401c42bf54b2750a23dd96611a58528905b2
ottocedeno <otto@creativespaceclub.com>
2020-11-12 20:29:49 -0500

    Added Form Submit component

---
+++ src/components/FormSubmitButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FormSubmitButton = () => {
+  return (
+    <input
+      type="submit"
+      value="Submit"
+      className="bg-red-600 text-white w-full py-3 rounded-full"
+    />
+  );
+};
+
+export default FormSubmitButton;
