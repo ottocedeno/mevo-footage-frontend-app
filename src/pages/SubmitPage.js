3932afa494465eb37fe4c51c31b4d73d473a6fd8
ottocedeno <otto@creativespaceclub.com>
2020-11-14 13:45:19 -0500

    Submit Page styling for MD breakpoint added

--- src/pages/SubmitPage.js
+++ src/pages/SubmitPage.js
@@ -4,17 +4,21 @@ import SubmitFormContainer from "../containers/SubmitFormContainer";
 const SubmitPage = () => {
   return (
     <div className="pb-10">
-      <div className="text-white px-4 py-10 bg-mevo-light-grey text-mevo-blue">
-        <h1 className="text-4xl font-bold leading-tight">
-          Submit your <br />
-          Mevo video
-        </h1>
-        <p className="mt-2">
-          Enter your Mevo Start footage to be featured on our video portal.
-          Chance to win a free Mevo Start every month!
-        </p>
+      <div className="text-white px-4 py-10 bg-mevo-light-grey text-mevo-blue flex justify-center">
+        <div className="max-w-lg ">
+          <h1 className="text-4xl font-bold leading-tight">
+            Submit your <br className="md:hidden" />
+            Mevo video
+          </h1>
+          <p className="mt-2">
+            Enter your Mevo Start footage to be featured on our video portal.
+            Chance to win a free Mevo Start every month!
+          </p>
+        </div>
       </div>
-      <SubmitFormContainer />
+      <div className="md:max-w-2xl md:m-auto">
+        <SubmitFormContainer />
+      </div>
     </div>
   );
 };
