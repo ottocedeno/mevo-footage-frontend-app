ced36b04e83a9dfe7cd1c052f3ed8a991e026557
ottocedeno <otto@creativespaceclub.com>
2020-11-14 14:00:26 -0500

    Fixed bug where submitData wasn't Returning fetch result

--- src/utilities/fetchData.js
+++ src/utilities/fetchData.js
@@ -1,5 +1,5 @@
 export const submitVideoData = (videoObject) => {
-  fetch("http://127.0.0.1:3000/videos", {
+  return fetch("http://127.0.0.1:3000/videos", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
