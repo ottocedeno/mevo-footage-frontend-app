9a1ab49056d29a6e9ee86026eeec74e5d1c8fad0
ottocedeno <otto@creativespaceclub.com>
2020-11-15 22:00:01 -0500

    Setup Redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers/rootReducer";
+
 import "./assets/main.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
