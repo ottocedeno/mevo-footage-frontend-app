60e9c46c54b9e075776d1c80f8cb069837cc6224
ottocedeno <otto@creativespaceclub.com>
2020-11-15 21:59:29 -0500

    LOAD_CATEGORIES action works

--- src/reducers/manageCategories.js
+++ src/reducers/manageCategories.js
@@ -1,7 +1,7 @@
 export default function manageCategories(categories = [], action) {
   switch (action.type) {
     case "LOAD_CATEGORIES":
-      return categories;
+      return (categories = action.categories);
     default:
       return categories;
   }
