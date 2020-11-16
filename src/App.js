989fd30cfc754177ba21e814e3ff8e9c182d3649
ottocedeno <otto@creativespaceclub.com>
2020-11-15 22:00:33 -0500

    Initial setup fo Category and Camera API loads

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import SubmitPage from "./pages/SubmitPage";
 
-function App() {
-  return <SubmitPage />;
+import { fetchCategoriesAndCameras } from "./utilities/fetchData";
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    return <SubmitPage />;
+  }
+
+  componentDidMount() {
+    fetchCategoriesAndCameras()
+      .then((response) => response.json())
+      .then((data) => {
+        this.props.loadCategories(data.categories);
+        this.props.loadCameras(data.cameras);
+      });
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadCategories: (categories) =>
+      dispatch({ type: "LOAD_CATEGORIES", categories }),
+    loadCameras: (cameras) => dispatch({ type: "LOAD_CAMERAS", cameras }),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
