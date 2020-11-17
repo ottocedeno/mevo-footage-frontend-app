7a20e0174435c6bf5f81d8799e4a304f73baa28c
ottocedeno <otto@creativespaceclub.com>
2020-11-16 20:44:18 -0500

    Refactored radio sections into component

---
+++ src/components/FormRadioGroup.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FormRadioInput from "../components/FormRadioInput";
+
+const FormRadioGroup = ({
+  title,
+  options,
+  name,
+  current,
+  handleFormInputChange,
+}) => {
+  return (
+    <div>
+      <p className="text-center uppercase font-bold text-sm">{title}</p>
+      <div className="flex flex-col mt-2 mb-4 pl-4">
+        {options.map((option, id) => {
+          return (
+            <FormRadioInput
+              key={id}
+              name={name}
+              value={option}
+              current={current}
+              handleFormInputChange={handleFormInputChange}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FormRadioGroup;
