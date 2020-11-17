2f889e15caba58004a881463f346e7ed49b8633a
ottocedeno <otto@creativespaceclub.com>
2020-11-16 20:44:39 -0500

    Refactored all text inputs into own component

---
+++ src/components/FormTextGroup.js
@@ -0,0 +1,41 @@
+import React from "react";
+import FormTextInput from "../components/FormTextInput";
+
+const FormInputGroup = ({ video, handleFormInputChange }) => {
+  return (
+    <div>
+      <FormTextInput
+        name="title"
+        placeholder="Video Title"
+        value={video.title}
+        handleFormInputChange={handleFormInputChange}
+      />
+      <FormTextInput
+        name="description"
+        placeholder="Quick Description"
+        value={video.description}
+        handleFormInputChange={handleFormInputChange}
+      />
+      <p className="text-sm text-center mb-2 text-mevo-dark-grey">
+        Please submit 1080p HD YouTube clip
+      </p>
+      <FormTextInput
+        name="youtube_url"
+        placeholder="YouTube Link"
+        value={video.youtube_url}
+        handleFormInputChange={handleFormInputChange}
+      />
+      <p className="text-sm text-center mb-2 text-mevo-dark-grey">
+        Emails are not shared publicly
+      </p>
+      <FormTextInput
+        name="user_email"
+        placeholder="Contact Email"
+        value={video.user_email}
+        handleFormInputChange={handleFormInputChange}
+      />
+    </div>
+  );
+};
+
+export default FormInputGroup;
