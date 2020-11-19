ed01714805283ceafe13950cbd824a16c92985d6
ottocedeno <otto@creativespaceclub.com>
2020-11-19 09:55:17 -0500

    Added confirmation page v1

---
+++ src/pages/ConfirmationPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PrimaryButton from "../components/UIelements/PrimaryButton";
+
+const ConfirmationPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-mevo-light-grey text-mevo-blue text-center px-8">
+      <h1 className="text-3xl font-bold leading-tight">
+        Thanks for submitting your video
+      </h1>
+      <p className="mt-6">
+        We'll review your submission shortly. <br />
+        In the meantime, check out other video submissions from our community
+      </p>
+      <div className="mt-6 w-full">
+        <PrimaryButton url="/submit-video" label="Back to Videos" />
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationPage;
