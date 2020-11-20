6052ca69fd5bfaf8a620b97ee5cf81856522e84b
ottocedeno <otto@creativespaceclub.com>
2020-11-19 21:36:12 -0500

    Initial stub of VideosPage

--- src/pages/VideosPage.js
+++ src/pages/VideosPage.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import PageHeader from "../components/UIelements/PageHeader";
 import PrimaryButton from "../components/UIelements/PrimaryButton";
+import VideoFilterContainer from "../containers/VideoFilterContainer";
 
 const VideosPage = () => {
   return (
@@ -12,8 +13,11 @@ const VideosPage = () => {
           Mevo videos shot by customers all around the world. Own a Mevo? Submit
           your footage for a chance to win free gear.
         </PageHeader.SubHeading>
-        <PrimaryButton label="Submit your footage" url="/submit-video" />
+        <div className="mt-4 w-3/5">
+          <PrimaryButton label="Submit your footage" url="/submit-video" />
+        </div>
       </PageHeader>
+      <VideoFilterContainer />
     </div>
   );
 };
