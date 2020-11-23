30bf5abf68404350010432763b392ce30d456955
ottocedeno <otto@creativespaceclub.com>
2020-11-23 14:34:39 -0500

    Added videos reducer to root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import manageCategories from "./manageCategories";
 import manageCameras from "./manageCameras";
+import manageVideos from "./manageVideos";
 
 const rootReducer = combineReducers({
   categories: manageCategories,
   cameras: manageCameras,
+  videos: manageVideos,
 });
 
 export default rootReducer;
