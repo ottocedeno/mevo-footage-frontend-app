9bb51f454786c462f6d52162b2f2243bc8ea40b1
ottocedeno <otto@creativespaceclub.com>
2020-11-24 12:47:22 -0500

    CSS clean up

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,7 +6,8 @@ const VideoCard = ({ videoData }) => {
     <div className="text-mevo-blue mb-10">
       <div className=" relative w-full" style={{ paddingBottom: "56.25%" }}>
         <iframe
-          className=" absolute top-0 left-0 w-full h-full border-none"
+          title={videoData.title}
+          className="absolute top-0 left-0 w-full h-full border-none"
           src={`https://www.youtube.com/embed/${youtubeVideoCode(
             videoData.youtube_url
           )}`}
@@ -16,7 +17,7 @@ const VideoCard = ({ videoData }) => {
       </div>
       <div className="px-4 mt-2">
         <h2 className="font-bold text-lg">{videoData.title}</h2>
-        <p className="mt-1">{videoData.description}</p>
+        <p className="">{videoData.description}</p>
         <p className="uppercase text-xs text-mevo-dark-grey font-bold mt-1">
           shot on {videoData.camera.model} - {videoData.category.name}
         </p>
